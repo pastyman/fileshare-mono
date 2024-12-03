d5bae509fb41823a02654009ac820eb1197f9822
Paris Val Baker <parisvalbaker@yahoo.co.uk>
2024-12-03 16:04:58 +0000

    add caching to ice endpoint

--- packages/filebump/src/pages/api/ice.ts
+++ packages/filebump/src/pages/api/ice.ts
@@ -1,19 +1,28 @@
 import axios from "axios"
 import { NextApiRequest, NextApiResponse } from "next"
 
+let cache = null as any
+let cacheTime = 0
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async function (req: NextApiRequest, res: NextApiResponse) {
 
-  const response = await axios.put(
-    "https://global.xirsys.net/_turn/sharefolder",
-    { "format": "urls", "expire": "120" },
-    {
-      headers: {
-        "Authorization": "Basic " + btoa("parisvb:563fe5de-77e3-11e9-9ec3-0242ac110003"),
-        "Content-Type": "application/json"
+  if (Date.now() - cacheTime > 300000) {
+    cache = null
+  }
+  if (!cache) {
+    const response = await axios.put(
+      "https://global.xirsys.net/_turn/sharefolder",
+      { "format": "urls", "expire": "360" },
+      {
+        headers: {
+          "Authorization": "Basic " + btoa("parisvb:563fe5de-77e3-11e9-9ec3-0242ac110003"),
+          "Content-Type": "application/json"
+        }
       }
-    }
-  )
+    )
+    cacheTime = Date.now()
+    cache = { ...response.data.v, cacheTime }
+  }
 
-  return res.status(200).json(response.data)
+  return res.status(200).json(cache)
 }
