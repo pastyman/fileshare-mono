6f8bf25ef03ac862d0660e05fe6ddf637eedc215
Paris Val Baker <parisvalbaker@yahoo.co.uk>
2024-12-16 16:48:11 +0000

    submit on enter

--- packages/filebump/src/pages/handshake-recieve.tsx
+++ packages/filebump/src/pages/handshake-recieve.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect } from "react"
-import { NextRouter, useRouter } from "next/router"
-import { Container, Spacer, Txt, StyledBox, Spinner, Loading, Input, ButtonAdd } from "ui-components"
+import { useState } from "react"
+import { useRouter } from "next/router"
+import { Container, Spacer, Txt, StyledBox, Input, ButtonAdd } from "ui-components"
 import { getUUID } from "helpers"
 import { serverConnectRecieve } from "rtc-client"
 
@@ -8,19 +8,18 @@ const Index = () => {
   const router = useRouter()
 
   const handleNavClick = (url: string, replace: boolean = false) => {
-    if (replace){
+    if (replace) {
       router.replace(url)
-    }
-    else {
+    } else {
       router.push(url)
     }
   }
 
-  //state
+  // State
   const [isSubmitting, setIsSubmitting] = useState(false)
   const [secret, setSecret] = useState("")
 
-  //get uuid
+  // Get UUID
   const clientId = getUUID()
 
   const handleSecretChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -28,40 +27,49 @@ const Index = () => {
     setSecret(value)
   }
 
-  //server funcs
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === "Enter") {
+      onSubmit()
+    }
+  }
+
+  // Server functions
   const onPeerId = (peerId: string | null) => {
-    setIsSubmitting(false);
+    setIsSubmitting(false)
 
     if (peerId) {
-      //navigate to recieve page
+      // Navigate to receive page
       handleNavClick(`/recieve?clientId=${clientId}&peerId=${peerId}`, true)
-    }
-    else {
-      //navigate to error page
+    } else {
+      // Navigate to error page
       handleNavClick(`/error`)
     }
   }
 
-  //send to server
+  // Send to server
   const scr = serverConnectRecieve(onPeerId)
 
   const onSubmit = async () => {
-    setIsSubmitting(true);
-    //execute query
+    setIsSubmitting(true)
+    // Execute query
     scr.connectRecieve(clientId, secret)
   }
 
   return (
     <Container uc="main">
-      <StyledBox
-        uc="solidBox"
-        ucHover="solidBoxHover"
-      >
+      <StyledBox uc="solidBox" ucHover="solidBoxHover">
         <Txt uc="boxHeading">Enter 6 digit PIN</Txt>
         <Spacer uc="medium" />
-        <Txt uc="boxTxt">Enter pin provided by sender to recieve your files</Txt>
+        <Txt uc="boxTxt">Enter pin provided by sender to receive your files</Txt>
         <Spacer uc="medium" />
-        <Input sx={{}} type="number" value={secret} onChange={handleSecretChange} placeholder="6 digit pin" />
+        <Input
+          sx={{}}
+          type="number"
+          value={secret}
+          onChange={handleSecretChange}
+          onKeyDown={handleKeyDown}
+          placeholder="6 digit pin"
+        />
         <Spacer uc="medium" />
         <Container uc="rowBox">
           <ButtonAdd
